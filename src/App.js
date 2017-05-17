27d543f4b9afa49018afbd2dc4245e1d0fc6c883
Paul Walsh <twentytwentyk@live.com>
2017-05-17 16:56:40 -0400

    It is time for tea challenge

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 // bring in React and Component from React
 
-import React, {Component} from 'react';
+import React, {Component} from 'react'; //This imports React methods and the Component class from the React library.
+
+
 
 // define our Hello component
-class Hello extends Component {
+class Hello extends Component { //This is the component we're creating. In this example, we are creating a "Hello" component. Component is the React library class we inherit from to create our component definitions. Here, we are creating a new Component subclass called Hello. Because it "extends" or "inherits from" Component, our Hello class gets to reuse code and capabilities from React.Component.
+
+
+
+
   // what should the component render?
   render () {
     // make sure to return some UI
     return (
-      <h1>Hello World!</h1>
+    <div>
+      <h1>It is time for tea.</h1>
+    </div>
     )
   }
 }
