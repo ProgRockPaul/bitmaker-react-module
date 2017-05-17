0ca15d613e9cf40bef8ab89f94abe4208754a574
Paul Walsh <twentytwentyk@live.com>
2017-05-17 16:53:14 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// bring in React and Component from React
+
+import React, {Component} from 'react';
+
+// define our Hello component
+class Hello extends Component {
+  // what should the component render?
+  render () {
+    // make sure to return some UI
+    return (
+      <h1>Hello World!</h1>
+    )
+  }
+}
+
+export default Hello
